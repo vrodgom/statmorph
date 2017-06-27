c114d3127a48e14e6dc688fa73f3b3c7670de739
vrodgom <vicente.rodgom@gmail.com>
2017-06-26 20:14:15 -0400

    Calculate Petrosian segmap and Gini coefficient.

--- statmorph.py
+++ statmorph.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy.optimize as opt
+import scipy.ndimage as ndi
+import astropy.convolution as conv
 from astropy.utils import lazyproperty
 import photutils
 
@@ -7,8 +9,8 @@ __all__ = ['SourceMorphology', 'source_morphology']
 
 class SourceMorphology(object):
     """
-    Class to measure morphological parameters of a single source.
-    The morphological parameters can be accessed as attributes or keys.
+    Class to measure the morphological parameters of a single labeled
+    source. The parameters can be accessed as attributes or keys.
 
     Parameters
     ----------
@@ -29,15 +31,35 @@ class SourceMorphology(object):
           radius, the mean flux at the edge of the aperture divided by
           the mean flux within the aperture is equal to ``eta``. The
           default value is typically set to 0.2 (Petrosian 1976).
+    petro_sigma_fraction : float, optional
+                           The fraction of the Petrosian radius used as
+                           a smoothing scale before defining the pixels
+                           that belong to the galaxy, at least for the
+                           calculation of the Gini coefficient. The
+                           default value is 0.2.
+    n_sigma_outlier : scalar, optional
+                      The number of standard deviations that define a
+                      pixel as an outlier, relative to its 8 neighbors.
+                      The default value is 10.
 
     References
     ----------
     Lotz J. M., Primack J., Madau P., 2004, AJ, 128, 163
 
     """
-    def __init__(self, image, segmap, label, mask=None, eta=0.2):
-        self.eta = eta
+    def __init__(self, image, segmap, label, mask=None, eta=0.2,
+                 petro_sigma_fraction=0.2, n_sigma_outlier=10):
+        self._eta = eta
+        self._petro_sigma_fraction = petro_sigma_fraction
+        self._n_sigma_outlier = n_sigma_outlier
         self._props = photutils.SourceProperties(image, segmap, label, mask=mask)
+
+        # Reference to the masked cutout used by photutils:
+        self._cutout = self._props._data_cutout_maskzeroed_double
+
+        # Centroid of the source relative to the cutout:
+        self._xc = self._props.xcentroid.value - self._props.xmin.value
+        self._yc = self._props.ycentroid.value - self._props.ymin.value
 
     def __getitem__(self, key):
         return getattr(self, key)
@@ -67,28 +89,48 @@ class SourceMorphology(object):
 
         """
         b = a / self._props.elongation.value
-        a_in = a - 1
-        a_out = a + 1
+        a_in = a - 1.0
+        a_out = a + 1.0
         b_out = a_out / self._props.elongation.value
         theta = self._props.orientation.value
 
-        # Centroid of the source relative to the cutout:
-        xc = self._props.xcentroid.value - self._props.xmin.value
-        yc = self._props.ycentroid.value - self._props.ymin.value
-
         ellip_annulus = photutils.EllipticalAnnulus(
-            (xc, yc), a_in, a_out, b_out, theta)
+            (self._xc, self._yc), a_in, a_out, b_out, theta)
         ellip_aperture = photutils.EllipticalAperture(
-            (xc, yc), a, b, theta)
+            (self._xc, self._yc), a, b, theta)
 
-        ellip_annulus_mean_flux = (ellip_annulus.do_photometry(
-            self._props._data_cutout_maskzeroed_double, method='exact')[0][0] /
-            ellip_annulus.area())
-        ellip_aperture_mean_flux = (ellip_aperture.do_photometry(
-            self._props._data_cutout_maskzeroed_double, method='exact')[0][0] /
-            ellip_aperture.area())
+        ellip_annulus_mean_flux = ellip_annulus.do_photometry(
+            self._cutout, method='exact')[0][0] / ellip_annulus.area()
+        ellip_aperture_mean_flux = ellip_aperture.do_photometry(
+            self._cutout, method='exact')[0][0] / ellip_aperture.area()
 
-        return ellip_annulus_mean_flux / ellip_aperture_mean_flux - self.eta
+        return ellip_annulus_mean_flux / ellip_aperture_mean_flux - self._eta
+
+    def _petrosian_function_circ(self, r):
+        """
+        Helper function to calculate ``petrosian_radius_circ``.
+        
+        For the circle with radius `r`, return the
+        ratio of the mean flux around the circle divided by
+        the mean flux within the circle, minus "eta" (eq. 4
+        from Lotz et al. 2004). The root of this function is
+        the Petrosian radius.
+
+        """
+        r_in = r - 1.0
+        r_out = r + 1.0
+
+        circ_annulus = photutils.CircularAnnulus(
+            (self._xc, self._yc), r_in, r_out)
+        circ_aperture = photutils.CircularAperture(
+            (self._xc, self._yc), r)
+
+        circ_annulus_mean_flux = circ_annulus.do_photometry(
+            self._cutout, method='exact')[0][0] / circ_annulus.area()
+        circ_aperture_mean_flux = circ_aperture.do_photometry(
+            self._cutout, method='exact')[0][0] / circ_aperture.area()
+
+        return circ_annulus_mean_flux / circ_aperture_mean_flux - self._eta
 
     @lazyproperty
     def petrosian_radius_ellip(self):
@@ -112,38 +154,6 @@ class SourceMorphology(object):
         
         return rpetro_ellip
 
-    def _petrosian_function_circ(self, r):
-        """
-        Helper function to calculate ``petrosian_radius_circ``.
-        
-        For the circle with radius `r`, return the
-        ratio of the mean flux around the circle divided by
-        the mean flux within the circle, minus "eta" (eq. 4
-        from Lotz et al. 2004). The root of this function is
-        the Petrosian radius.
-
-        """
-        r_in = r - 1
-        r_out = r + 1
-
-        # Centroid of the source relative to the cutout:
-        xc = self._props.xcentroid.value - self._props.xmin.value
-        yc = self._props.ycentroid.value - self._props.ymin.value
-
-        circ_annulus = photutils.CircularAnnulus(
-            (xc, yc), r_in, r_out)
-        circ_aperture = photutils.CircularAperture(
-            (xc, yc), r)
-
-        circ_annulus_mean_flux = (circ_annulus.do_photometry(
-            self._props._data_cutout_maskzeroed_double, method='exact')[0][0] /
-            circ_annulus.area())
-        circ_aperture_mean_flux = (circ_aperture.do_photometry(
-            self._props._data_cutout_maskzeroed_double, method='exact')[0][0] /
-            circ_aperture.area())
-
-        return circ_annulus_mean_flux / circ_aperture_mean_flux - self.eta
-
     @lazyproperty
     def petrosian_radius_circ(self):
         """
@@ -165,10 +175,89 @@ class SourceMorphology(object):
         
         return rpetro_circ
 
+    @lazyproperty
+    def _cutout_clean(self):
+        """
+        Remove outliers from the cutout containing the source.
+        
+        """
+        local_footprint = np.array([
+            [1, 1, 1],
+            [1, 0, 1],
+            [1, 1, 1],
+        ])
+        local_mean = ndi.filters.generic_filter(
+            self._cutout, np.mean, footprint=local_footprint)
+        local_sigma = ndi.filters.generic_filter(
+            self._cutout, np.std, footprint=local_footprint)
+        cutout_clean = np.where(
+            self._cutout < local_mean + self._n_sigma_outlier * local_sigma,
+            self._cutout, 0)
+
+        return cutout_clean
+
+    @lazyproperty
+    def _petrosian_segmap(self):
+        """
+        Create a segmentation map (relative to the source cutout)
+        based on the Petrosian "radius".
+        
+        Notes
+        -----
+        For simplicity, we tentatively remove the condition that the
+        smoothing scale be at least 3 times the PSF scale, which is not
+        mentioned in the original paper. We also remove outliers *before*
+        smoothing the image.
+        
+        """
+        # Smooth image (if too slow, try scipy.fftconvolve)
+        petro_sigma = self._petro_sigma_fraction * self.petrosian_radius_ellip
+        cutout_smooth = ndi.gaussian_filter(self._cutout_clean, petro_sigma)
+        
+        # Use mean flux at the Petrosian "radius" as threshold
+        a_in = self.petrosian_radius_ellip - 1.0
+        a_out = self.petrosian_radius_ellip + 1.0
+        b_out = a_out / self._props.elongation.value
+        theta = self._props.orientation.value
+        ellip_annulus = photutils.EllipticalAnnulus(
+            (self._xc, self._yc), a_in, a_out, b_out, theta)
+        ellip_annulus_mean_flux = ellip_annulus.do_photometry(
+            cutout_smooth, method='exact')[0][0] / ellip_annulus.area()
+        petro_segmap = np.where(cutout_smooth >= ellip_annulus_mean_flux, 1, 0)
+        
+        return petro_segmap
+
+    @lazyproperty
+    def gini(self):
+        """
+        Calculate the Gini coefficient as described in Lotz et al. (2004).
+        
+        Notes
+        -----
+        The Gini calculation is ambiguous when the image has negative values
+        (e.g., from sky subtraction). Although this function considers the
+        absolute value of the pixels, the negative values in the data have
+        already been set to zero.
+        
+        """
+        image = self._cutout_clean.flatten()
+        segmap = self._petrosian_segmap.flatten()
+
+        sorted_pixelvals = np.sort(np.abs(image[segmap == 1]))
+        total_absflux = np.sum(sorted_pixelvals)
+
+        # Fast computation using array operations
+        n = len(sorted_pixelvals)
+        indices = np.arange(1, n+1, dtype=np.int64)  # start at i=1
+        gini = np.sum((2*indices-n-1)*sorted_pixelvals) / (total_absflux*float(n-1))
+
+        return gini
+
 
 def source_morphology(image, segmap):
     """
-    Calculate morphological parameters of all sources defined by segmap.
+    Calculate the morphological parameters of all sources in ``image``
+    as defined by ``segmap``.
     
     Parameters
     ----------
@@ -187,6 +276,16 @@ def source_morphology(image, segmap):
           radius, the mean flux at the edge of the aperture divided by
           the mean flux within the aperture is equal to ``eta``. The
           default value is typically set to 0.2 (Petrosian 1976).
+    petro_sigma_fraction : float, optional
+                           The fraction of the Petrosian radius used as
+                           a smoothing scale before defining the pixels
+                           that belong to the galaxy, at least for the
+                           calculation of the Gini coefficient. The
+                           default value is 0.2.
+    n_sigma_outlier : scalar, optional
+                      The number of standard deviations that define a
+                      pixel as an outlier, relative to its 8 neighbors.
+                      The default value is 10.
 
     Returns
     -------
@@ -209,46 +308,6 @@ def source_morphology(image, segmap):
         sources_morph.append(SourceMorphology(image, segmap, label))
 
     return sources_morph
-
-
-def compute_gini(image, segmap):
-    """
-    Calculate the Gini coefficient as in eq. (3) from Lotz et al. (2004).
-    
-    Parameters
-    ----------
-    image : array_like
-            Image to be analyzed.
-    segmap : array_like
-            Segmentation map with the same shape as "image". The main
-            galaxy segment should be indicated with 1; other values are 0.
-
-    Returns
-    -------
-    gini : float
-           The computed Gini value.
-
-    Notes
-    -----
-    The Gini calculation is ambiguous when the image has negative values
-    (e.g., from sky subtraction). In these cases we consider the
-    absolute value of negative pixels, even if this means that the
-    ordering of pixels with small values is not perfectly conserved.
-    
-    """
-    image = np.asarray(image).flatten()
-    segmap = np.asarray(segmap).flatten()
-
-    sorted_pixelvals = np.sort(np.abs(image[segmap == 1]))
-    total_absflux = np.sum(sorted_pixelvals)
-
-    # Fast computation using numpy arrays
-    n = len(sorted_pixelvals)
-    indices = np.arange(1, n+1, dtype=np.int64)  # start at i=1
-    gini = np.sum((2*indices-n-1)*sorted_pixelvals) / (total_absflux*float(n-1))
-
-    return gini
-
 
 
 
