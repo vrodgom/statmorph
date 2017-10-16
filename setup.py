61e3c74797091424026499d347a668aaaf55dbd1
vrodgom <vicente.rodgom@gmail.com>
2017-10-16 18:19:18 -0400

    Create setup.py and MANIFEST.in files.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='statmorph',
+    version='0.1.0',
+    description='Non-parametric morphological diagnostics of galaxy images',
+    long_description=long_description,
+    url='https://github.com/vrodgom/statmorph',
+    author='Vicente Rodriguez-Gomez',
+    author_email='vrg@jhu.edu',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='galaxy morphology extragalactic astronomy',
+    packages=['statmorph'],
+    install_requires=['scikit-image>=0.13',
+                      'astropy>=2.0',
+                      'photutils>=0.3'],
+)
