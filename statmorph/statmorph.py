96b01f3a1b09d30f4d2d9f9d2cec167154aad06b
Vicente Rodriguez-Gomez <vicente.rodgom@gmail.com>
2018-08-29 02:22:39 -0400

    Prepare for statmorph v0.3.2.

--- statmorph/statmorph.py
+++ statmorph/statmorph.py
@@ -25,7 +25,7 @@ import photutils
 __all__ = ['ConvolvedSersic2D', 'SourceMorphology', 'source_morphology',
            '__version__']
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 def _quantile(sorted_values, q):
     """
