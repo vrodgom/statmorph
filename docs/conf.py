76f8bfccbcc668f75ef3f082362d9f0fe26d0743
Vicente Rodriguez-Gomez <vicente.rodgom@gmail.com>
2025-07-02 13:00:49 -0600

    Version 0.7.0.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2017-2025, Vicente Rodriguez-Gomez'
 author = 'Vicente Rodriguez-Gomez'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.7.0'
 
 
 # -- General configuration ---------------------------------------------------
